07cdefaa9bc969e08d7402172b6cdd029fd29550
pawel.urbanowicz <pawel.urbanowicz@polidea.com>
2020-12-11 16:14:38 +0100

    feat: setup of spring boot helloworld

---
+++ src/main/java/com/urban/ActionMonitor/ActionMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.urban.ActionMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionMonitorApplication.class, args);
+	}
+
+}
